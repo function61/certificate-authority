601f2ef8019a4d781385db5365fa2053b23eb3d2
joonas.fi <pompo500@xs.fi>
2017-01-09 13:58:38 +0200

    Switch to using ECDSA

--- ca/init_ca.sh
+++ ca/init_ca.sh
@@ -1,5 +1,7 @@
 #!/bin/sh -eu
 
+# why ECDSA? => https://blog.cloudflare.com/ecdsa-the-digital-signature-algorithm-of-a-better-internet/
+
 cfssl gencert -initca ca/ca-csr.json | cfssljson -bare ca/ca -
 
 cp ca/ca.pem ca/ca.crt
