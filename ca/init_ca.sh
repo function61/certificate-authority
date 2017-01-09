96f9cabe139d61fe32b24b848c350d60702ffada
joonas.fi <pompo500@xs.fi>
2017-01-09 14:53:28 +0200

    This was already explained in README

--- ca/init_ca.sh
+++ ca/init_ca.sh
@@ -1,7 +1,5 @@
 #!/bin/sh -eu
 
-# why ECDSA? => https://blog.cloudflare.com/ecdsa-the-digital-signature-algorithm-of-a-better-internet/
-
 cfssl gencert -initca ca/ca-csr.json | cfssljson -bare ca/ca -
 
 mv ca/ca.pem ca/ca.crt
