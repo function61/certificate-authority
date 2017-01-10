07e9425f719294698a2d8a438cdba66f5375cbb4
joonas.fi <pompo500@xs.fi>
2017-01-10 16:02:18 +0200

    Rename server-profile.json to server-csr.json

--- server-signer/sign.sh
+++ server-signer/sign.sh
@@ -3,7 +3,7 @@
 cfssl gencert \
 	-ca=ca/ca.crt -ca-key=ca/ca.key -config=ca/ca-config.json \
 	-profile=server \
-	server-signer/server-profile.json | cfssljson -bare server-signer/signed/server
+	server-signer/server-csr.json | cfssljson -bare server-signer/signed/server
 
 mv server-signer/signed/server.pem server-signer/signed/server.crt
 mv server-signer/signed/server-key.pem server-signer/signed/server.key
